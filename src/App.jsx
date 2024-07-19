d9d2f483b086a82ca0a12f63477e8061442e0e59
unknown <MATHEUS0376@digitalcollegesul.local>
2024-07-19 17:11:26 -0300

    Footer em construcao

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+
+import "./../public/styles/default.css";
 import { Header } from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer"; 
 
 export function App() {
     return (
         <>
             <Header />
+
+            <Footer />
         </>
     );
 }
