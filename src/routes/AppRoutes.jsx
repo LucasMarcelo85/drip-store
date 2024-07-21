f885fd9071cd9a70db16a919de7060a0b1a8bdfe
DevSaLLein <isaac.andrade3@aluno.ce.gov.br>
2024-07-21 15:18:31 -0300

    styles: edit weight font from title product ProductCard

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from "../pages/HomePage";
 import { ProductListeningPage } from "../pages/productListingPage";
 import { ProductViewPage } from "../pages/ProductViewPage";
+import { NotFoundPage } from "../pages/NotFoundPage";
 
 export function AppRoutes() {
     return (
@@ -11,6 +12,7 @@ export function AppRoutes() {
                 <Route index element={<HomePage />} />
                 <Route path="/produtos" element={<ProductListeningPage />} />
                 <Route path="/produto" element={<ProductViewPage />} />
+                <Route path="*" element={<NotFoundPage />} />
             </Routes>
         </BrowserRouter>
     );
