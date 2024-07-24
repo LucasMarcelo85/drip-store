bde085ee9cbd3703e1c89c6228ab01d63fc2b8f5
unknown <ISAAC6284@digitalcollegesul.local>
2024-07-24 15:07:24 -0300

    initial producao produtos page

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,16 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { HomePage } from "../pages/HomePage";
-import { ProductListeningPage } from "../pages/productListingPage";
 import { ProductViewPage } from "../pages/ProductViewPage";
 import { NotFoundPage } from "../pages/NotFoundPage";
+import { Products } from "../pages/Products/Products";
 
 export function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route index element={<HomePage />} />
-                <Route path="/produtos" element={<ProductListeningPage />} />
+                <Route path="/produtos" element={<Products />} />
                 <Route path="/produto" element={<ProductViewPage />} />
                 <Route path="*" element={<NotFoundPage />} />
             </Routes>
