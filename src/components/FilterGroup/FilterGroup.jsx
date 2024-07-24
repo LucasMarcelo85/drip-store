bb0f2929d41c2a37deb739972c69aa8fbd5b55b5
DevSaLLein <isaac.andrade3@aluno.ce.gov.br>
2024-07-24 18:29:44 -0300

    filter feito

--- src/components/FilterGroup/FilterGroup.jsx
+++ src/components/FilterGroup/FilterGroup.jsx
@@ -5,107 +5,104 @@ export function FilterGroup() {
         <aside>
             <div className="side">
                 <div className="filtroprods">
-                    <h3>Filtrar por</h3>
 
-                    <hr />
+                    <h4 className="dcima">Filtrar por</h4>
+
+                    <hr className="linha" />
 
                     <div className="markas">
-                        <h3>Marka</h3>
+                        <h4>Marka</h4>
 
                         <form>
-                            <input
-                                type="checkbox"
-                                id="addidas"
-                                value="ADDIDAS"
-                            />
-                            <label htmlFor="addidas">Addidas</label>
+                            <div className="addi">
+                                <input type="checkbox" id="addidas" value="ADDIDAS" />
+                                <label htmlFor="addidas">Addidas</label>
+                            </div>
 
-                            <input
-                                type="checkbox"
-                                id="calenciga"
-                                value="CALENCIAGA"
-                            />
-                            <label htmlFor="calenciaga">Calenciaga</label>
+                            <div className="ciaga">
+                                <input type="checkbox" id="calenciga" value="CALENCIAGA" />
+                                <label htmlFor="calenciaga">Calenciaga</label>
+                            </div>
 
-                            <input
-                                type="checkbox"
-                                id="k-swiss"
-                                value="K-SWISS"
-                            />
-                            <label htmlFor="k-swiss">K-Swiss</label>
+                            <div className="wiss">
+                                <input type="checkbox" id="k-swiss" value="K-SWISS" />
+                                <label htmlFor="k-swiss">K-Swiss</label>
+                            </div>
 
-                            <input type="checkbox" id="nike" value="NIKE" />
-                            <label htmlFor="nike">Nike</label>
+                            <div className="like">
+                                <input type="checkbox" id="nike" value="NIKE" />
+                                <label htmlFor="nike">Nike</label>
+                            </div>
 
-                            <input type="checkbox" id="puma" value="PUMA" />
-                            <label htmlFor="puma">Puma</label>
+                            <div className="juma">
+                                <input type="checkbox" id="puma" value="PUMA" />
+                                <label htmlFor="puma">Puma</label>
+                            </div>
+
                         </form>
                     </div>
 
                     <div className="catingoria">
-                        <h3>Categoria</h3>
+                        <h4>Categoria</h4>
 
                         <form>
-                            <input
-                                type="checkbox"
-                                id="esporte-e-lazer"
-                                value="ESPORTES-E-LAZER"
-                            />
-                            <label htmlFor="esporte-e-lazer">
-                                Esportes e lazer
-                            </label>
+                            <div className="sportez">
+                                <input type="checkbox" id="esporte-e-lazer" value="ESPORTES-E-LAZER" />
+                                <label htmlFor="esporte-e-lazer">Esportes e lazer</label>
+                            </div>
 
-                            <input type="checkbox" id="casual" value="CASUAL" />
-                            <label htmlFor="casual">Casual</label>
+                            <div className="casa">
+                                <input type="checkbox" id="casual" value="CASUAL" />
+                                <label htmlFor="casual">Casual</label>
+                            </div>
 
-                            <input
-                                type="checkbox"
-                                id="utilitario"
-                                value="UTILITARIO"
-                            />
-                            <label htmlFor="utilitario">Utilitário</label>
+                            <div className="util">
+                                <input type="checkbox" id="utilitario" value="UTILITARIO" />
+                                <label htmlFor="utilitario">Utilitário</label>
+                            </div>
 
-                            <input
-                                type="checkbox"
-                                id="corrida"
-                                value="CORRIDA"
-                            />
-                            <label htmlFor="corrida">Corrida</label>
+                            <div className="racha">
+                                <input type="checkbox" id="corrida" value="CORRIDA" />
+                                <label htmlFor="corrida">Corrida</label>
+                            </div>
                         </form>
                     </div>
 
                     <div className="gen">
-                        <h3>Gênero</h3>
+                        <h4>Gênero</h4>
 
                         <form>
-                            <input
-                                type="checkbox"
-                                id="masculino"
-                                value="MASCULINO"
-                            />
-                            <label htmlFor="masculino">Masculino</label>
+                            <div className="masc">
+                                <input type="checkbox" id="masculino" value="MASCULINO" />
+                                <label htmlFor="masculino">Masculino</label>
+                            </div>
 
-                            <input
-                                type="checkbox"
-                                id="feminino"
-                                value="FEMININO"
-                            />
-                            <label htmlFor="feminino">Feminino</label>
+                            <div className="femea">
+                                <input type="checkbox" id="feminino" value="FEMININO" />
+                                <label htmlFor="feminino">Feminino</label>
+                            </div>
 
-                            <input type="checkbox" id="unisex" value="UNISEX" />
-                            <label htmlFor="unisex">Unisex</label>
+                            <div className="unisex">
+                                <input type="checkbox" id="unisex" value="UNISEX" />
+                                <label htmlFor="unisex">Unisex</label>
+                            </div>
                         </form>
                     </div>
 
                     <div className="stado">
-                        <h3>Estado</h3>
+                        <h4>Estado</h4>
 
                         <form>
-                            <input type="radio" id="novo" value="NOVO" />
-                            <label htmlFor="novo">Novo</label>
+                            <div className="nuevo">
+                                <input type="radio" id="novo" value="NOVO" />
+                                <label htmlFor="novo">Novo</label>
+                            </div>
 
-                            <input type="radio" id="usado" value="USADO" />
-                            <label htmlFor="usado">Usado</label>
+                            <div className="vei">
+                                <input type="radio" id="usado" value="USADO" />
+                                <label htmlFor="usado">Usado</label>
+                            </div>
+
                         </form>
                     </div>
                 </div>
