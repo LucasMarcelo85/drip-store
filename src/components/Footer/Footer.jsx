aa9df98fb47f1b8b6925189b98450a61bb3e96fa
Marcelo Souza <lucasmarcelo8585@gmail.com>
2024-08-22 13:04:03 -0300

    att. responsividade

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,76 +7,64 @@ import "./Footer.css";
 
 export function Footer() {
     return (
-        <footer className="footer">
-            {/* div para reposividade */}
-            <div className="resp">
-                {/* div geral */}
-                <div className="geral">
-                    <div className="container">
-                        {/* div das logos */}
-                        <div className="dig-logo">
-                            <img src={footerLogo} alt="Logo Digital Store" />
-                            <p>
-                                Lorem ipsum dolor sit, amet consectetur adipisicing
-                                elit. Odio mollitia temporibus modi est qui distinctio!
-                            </p>
-                            <div className="redesLogo">
+        <footer>
+
+<footer class="footer">
+    
+    <div class="footer-logo">
+        
+        <h2><img src={footerLogo} alt="Logo Digital Store" /></h2>
+        
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique ut delectus ratione distinctio asperiores totam deleniti cum fugit dolorem beatae.</p>
+
+        <div className="redesLogo">
                                 <img src={fbkLogo} alt="Logo Facebook" />
                                 <img src={instaLogo} alt="Logo Instagram" />
                                 <img src={ttLogo} alt="Logo Twitter" />
                             </div>
-                        </div>
-                        {/* div das logos */}
-                        {/* div das informações */}
-                        <div className="container-infos">
+                            
+    </div>
+    
+    <div class="footer-links">
+        
+        <h3>Informação</h3>
+        <ul>
+            <li><a href="#">Sobre Drip Store</a></li>
+            <li><a href="#">Segurança</a></li>
+            <li><a href="#">Wishlist</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Trabalhe conosco</a></li>
+            <li><a href="#">Meus Pedidos</a></li>
+        </ul>
+    </div>
+    <div class="footer-links">
+        <h3>Categorias</h3>
+        <ul>
+            <li><a href="#">Camisetas</a></li>
+            <li><a href="#">Calças</a></li>
+            <li><a href="#">Bonés</a></li>
+            <li><a href="#">Headphones</a></li>
+            <li><a href="#">Tênis</a></li>
+        </ul>
+    </div>
+    <div class="footer-contact">
+        <h3>Contato</h3>
+        <p>Av. Santos Dumont, 1510 - 1 andar - Aldeota, Fortaleza - CE, 60150-161</p>
+        <p>(85) 3051-3411</p>
+    </div>
+    
+</footer>
 
-                            <div className="listainfos1">
-                                <h3>Informação</h3>
-                                <ul>
-                                    <li>Sobre Drip Store</li>
-                                    <li>Segurança</li>
-                                    <li>Wishlist</li>
-                                    <li>Blog</li>
-                                    <li>Trabalhe conosco</li>
-                                    <li>Meus Pedidos</li>
-                                </ul>
-                            </div>
-
-                            <div className="listainfos2">
-                                <h3>Categorias</h3>
-                                <ul>
-                                    <li>Camisetas</li>
-                                    <li>Calças</li>
-                                    <li>Bonés</li>
-                                    <li>Headphones</li>
-                                    <li>Tênis</li>
-                                </ul>
-                            </div>
-
-                            <div className="listainfos3">
-                                <h3>Contato</h3>
-                                <ul>
-                                    <p>
-                                        Av. Santos Dumont, 1510 - 1 andar - Aldeota,
-                                        Fortaleza - CE, 60150-161
-                                    </p>
-                                    <li>(85) 3051-3411</li>
-                                </ul>
-                            </div>
-                        </div>
-                        {/* div das informações */}
-                    </div>
-                    {/* div da linha de baixo */}
-                    <div className="digcoll">
+<div className="digcoll">
                         <hr />
                         <p>@ 2022 Digital College</p>
                     </div>
-                    {/* div da linha de baixo */}
-                </div>
-                {/* div geral */}
-
-            </div>
-            {/* div para reposividade */}
+                 
+        
+                            
+                           
+                          
+                       
         </footer>
     );
 }
